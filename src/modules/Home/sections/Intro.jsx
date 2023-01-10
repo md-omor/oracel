3b7d5dd331ba419fdc2374a48f265d586f455f06
md-omor <developeromor@gmail.com>
2023-01-11 00:39:56 +0600

    create intro section

--- src/modules/Home/sections/Intro.jsx
+++ src/modules/Home/sections/Intro.jsx
@@ -10,29 +10,6 @@ const Intro = () => {
       <div className="container mx-auto mb-5">
         <IntroInfo />
       </div>
-      <div className="mt-[130px] introBg w-full h-[1000px] overflow-hidden">
-        <div className="container mx-auto pt-[110px] mb-5">
-          <h1 className="font-primary font-semibold text-[38px] leading-[48px] mb-[30px] text-white">
-            Meet Oracle NFTs ⚡
-          </h1>
-          <div className="relative">
-            <div className="w-full h-[600px]">
-              <img
-                src="/images/introLinear.svg"
-                alt="video"
-                className="w-full h-full"
-              />
-            </div>
-            <div className="w-[110px] h-[110px]">
-              <img
-                src="/images/play.svg"
-                alt="play"
-                className="w-full h-full"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
