9036fa3c95c6310f78a11a96b82754d0761b99dc
md-omor <developeromor@gmail.com>
2023-01-14 01:19:56 +0600

    soem changes in drops section

--- src/modules/Home/sections/Drops.jsx
+++ src/modules/Home/sections/Drops.jsx
@@ -22,6 +22,20 @@ const Drops = () => {
           pagination={{
             clickable: true,
           }}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            1140: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
           modules={[Navigation, Pagination]}
         >
           {dropsInfo?.map((data, index) => (
