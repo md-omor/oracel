913f1016110618d549baa937865afbce87a57648
md-omor <developeromor@gmail.com>
2023-01-25 05:49:57 +0600

    change intro section with responsive

--- src/modules/Home/sections/Intro.jsx
+++ src/modules/Home/sections/Intro.jsx
@@ -4,7 +4,7 @@ import IntroInfo from "../partials/IntroInfo";
 const Intro = () => {
   return (
     <div className="pt-[110px]">
-      <h1 className="font-primary font-semibold text-[200px] leading-[90%] introHeader mb-10 uppercase text-center overflow-hidden text-clip whitespace-nowrap	">
+      <h1 className="font-primary hidden md:flex font-semibold text-[200px] leading-[90%] introHeader mb-10 uppercase text-center overflow-hidden text-clip whitespace-nowrap	">
         Create & sell your NFTs
       </h1>
       <div className="container mx-auto mb-5">
