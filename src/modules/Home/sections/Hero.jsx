0a7cdb315a278763d17978efabbafce9282f3f0e
md-omor <developeromor@gmail.com>
2023-01-25 05:49:44 +0600

    change hero section with responsive

--- src/modules/Home/sections/Hero.jsx
+++ src/modules/Home/sections/Hero.jsx
@@ -4,36 +4,36 @@ const Hero = () => {
   return (
     <div className="container mx-auto flex flex-wrap justify-between items-center h-[970px]">
       <div className="relative">
-        <div className="w-[670px] h-[670px]">
+        <div className="md:w-[670px] md:h-[670px] w-[400px] h-[410px]">
           <img
             src="/images/herobg.svg"
             alt="hero bg"
             className="w-full h-full"
           />
         </div>
-        <div className="w-[635px] h-[635px] bg-[#161616] rounded-[10px] flex flex-col items-center top-[16px] left-[17px] absolute p-5">
-          <div className="w-[600px] h-[500px]">
+        <div className="md:w-[635px] md:h-[635px] w-[378px] h-[390px] bg-[#161616] rounded-[10px] flex flex-col items-center top-[16px] left-2 md:left-[17px] absolute p-4 md:p-5">
+          <div className="md:w-[600px] md:h-[500px] w-[344px] h-[286px] rounded-full">
             <img
               src="/images/heroimage.svg"
               alt="mask"
               className="w-full h-full"
             />
           </div>
-          <div className="w-full items-start my-[20px]">
-            <h1 className="font-primary font-semibold text-2xl text-white ">
+          <div className="w-full items-start md:my-5 mt-4">
+            <h1 className="font-primary font-semibold text-base md:text-2xl text-white ">
               Alfa Romeo’s C43 (2023)
             </h1>
-            <div className="flex items-center justify-between ">
+            <div className="flex items-center justify-between mt-2">
               <div className="flex gap-[10px] items-center">
-                <div className="w-[35px] relative">
-                  <div className="w-[30px] h-[30px]">
+                <div className="md:w-[35px] w-5 relative">
+                  <div className="md:w-[30px] md:h-[30px] w-4 h-4">
                     <img
                       src="/images/profilemask.svg"
                       alt="profile mask"
                       className="w-full h-full"
                     />
                   </div>
-                  <div className="w-3 h-3 absolute right-1 -bottom-[2px]">
+                  <div className="md:w-3 md:h-3 w-[7px] h-[7px] absolute right-1 -bottom-[2px]">
                     <img
                       src="/images/tickmark.svg"
                       alt="tick mark"
@@ -41,7 +41,7 @@ const Hero = () => {
                     />
                   </div>
                 </div>
-                <h5 className="font-primary font-medium text-[15px] leading-[20px] text-white">
+                <h5 className="font-primary font-medium text-xs md:text-[15px] md:leading-[20px] text-white">
                   @F1nfts
                 </h5>
               </div>
@@ -57,8 +57,8 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="2xl:m-0 my-10">
-        <h1 className="font-primary font-semibold text-[55px] leading-[64px] text-white md:w-[660px] mb-5">
+      <div className="2xl:m-0 md:my-10">
+        <h1 className="font-primary font-semibold md:text-[55px] md:leading-[64px]  text-4xl text-white md:w-[660px] mb-5">
           Alfa Romeo 2023, Buy NFT now
         </h1>
         <p className="font-primary font-semibold text-[19px] leading-[28px] text-[#969696] md:w-[660px]">
